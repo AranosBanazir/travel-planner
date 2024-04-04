b6072109e30692e8f6fef199ea385f63dcae6d73
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-04-04 03:37:43 -0500

    added styles to map markers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
 const key = 'a9d59121cb4743ac9edb7c6853265cb9'
 
-const options = $('input:checked')
 
-console.log(options.dataset)
+function getCheckedItems(){
+    const options = $('input:checked')
+    const categories = []
+    for (let i = 0; i< options.length; i++){
+        categories.push(options[i].dataset.category)
+    }
+    
+   return categories.toString()
+
+}
+
 //function to get geoid: use https://api.geoapify.com/v1/geocode
     function getGeoId(place){
         fetch(`https://api.geoapify.com/v1/geocode/search?text=${place}&apiKey=${key}`)
@@ -23,9 +32,9 @@ console.log(options.dataset)
 
 function getLocalInformation(id){
 
-     
+     const categories = getCheckedItems()
 
-fetch(`https://api.geoapify.com/v2/places?categories=${categories}&filter=place:${id}&apiKey=${key}`)
+fetch(`https://api.geoapify.com/v2/places?categories=${categories}&filter=place:${id}&limit=50&apiKey=${key}`)
 .then(function(response){
     return response.json()
 })
@@ -33,17 +42,42 @@ fetch(`https://api.geoapify.com/v2/places?categories=${categories}&filter=place:
     const locations = data.features
     console.log(locations)
     for(let i=0; i < locations.length; i++){
+        const categories = locations[i].properties.categories
         if (locations[i].properties.name === undefined){
-            console.log(locations[i].properties.address_line1)
-            newMarker(`${locations[i].properties.address_line1}`, 'feature', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+
+
+                        
+            if (categories.includes('healthcare')){
+                newMarker(`${locations[i].properties.address_line1}`, 'healthcare', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('catering')){
+                newMarker(`${locations[i].properties.address_line1}`, 'catering', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('accommodation')){
+                newMarker(`${locations[i].properties.address_line1}`, 'accommodation', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('airport')){
+                newMarker(`${locations[i].properties.address_line1}`, 'airport', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('entertainment')){
+                newMarker(`${locations[i].properties.address_line1}`, 'entertainment', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }
         }else{
-        console.log(locations[i].properties.name)
-        newMarker(`${locations[i].properties.name}`, 'feature', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+        //setting the type based on properties.categories containing the original types
+            
+            if (categories.includes('healthcare')){
+                newMarker(`${locations[i].properties.name}`, 'healthcare', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('catering')){
+                newMarker(`${locations[i].properties.name}`, 'catering', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('accommodation')){
+                newMarker(`${locations[i].properties.name}`, 'accommodation', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('airport')){
+                newMarker(`${locations[i].properties.name}`, 'airport', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }else if (categories.includes('entertainment')){
+                newMarker(`${locations[i].properties.name}`, 'entertainment', locations[i].geometry.coordinates[1], locations[i].geometry.coordinates[0] )
+            }
         }
     }
 })
 
 }
+
 
 
 let map;
@@ -56,12 +90,13 @@ async function initMap(lat, lon) {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 13,
     center: position,
     mapId: "123456",
   });
 
-
+  const mapEl = $('#map')
+  mapEl.css('border-radius', '10px')
 }
 
 let markercount = 1
@@ -76,10 +111,44 @@ async function newMarker(location, type = 'feature', lat, lon){
             style = {
                 background: 'red',
                 borderColor: 'black',
-                glyph: `${markercount++}`,   
+                glyph: `${markercount}`,
+                glyphColor: 'white',   
             }
-        }else if (type === 'c'){
-
+        }else if (type === 'catering'){
+            style = {
+                background: 'blue',
+                borderColor: 'white',
+                glyph: `${markercount}`,
+                glyphColor: 'white',   
+            }
+        }else if (type === 'healthcare'){
+            style = {
+                background: 'green',
+                borderColor: 'black',
+                glyph: `${markercount}`,
+                glyphColor: 'white',   
+            }
+        }else if (type === 'airport'){
+            style = {
+                background: 'pink',
+                borderColor: 'black',
+                glyph: `${markercount}`,
+                glyphColor: 'white',   
+            }
+        }else if (type === 'entertainment'){
+            style = {
+                background: 'purple',
+                borderColor: 'black',
+                glyph: `${markercount}`,
+                glyphColor: 'white',   
+            }
+        }else if (type === 'accommodation'){
+            style = {
+                background: 'white',
+                borderColor: 'black',
+                glyph: `${markercount}`,
+                glyphColor: 'black',
+            }
         }
 
 
@@ -97,14 +166,25 @@ async function newMarker(location, type = 'feature', lat, lon){
 
    
   });
+
+  const list = $('#places')
+  const place = $(`<li>     ${markercount}. ${location}</li>`)
+  place.css('background-color', style.background)
+  place.css('color', style.glyphColor)
+  place.appendTo(list)
+ 
+  markercount++
 }
 
 function handleSubmit(e){
     const search = document.getElementById('search')
     const map = document.getElementById('map')
+    const ol = document.getElementById('places')
     e.preventDefault()
     getGeoId(search.value)
+    ol.innerHTML = ''
     map.innerHTML = ''
+    markercount = 1
 }
 
 document.querySelector('form').addEventListener('submit', handleSubmit)
