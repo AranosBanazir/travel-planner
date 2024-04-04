3c345ba932b7e38499406bd8d7d43b2652555b85
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-04-03 23:19:22 -0500

    setting up fetch requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,38 @@
 const key = 'a9d59121cb4743ac9edb7c6853265cb9'
-const url = 'https://api.geoapify.com/v2/places?'
 
-fetch(`${url}categories=airport,healthcare&filter=place:519951e2299a6f56c0590e7ff21c36bc4540f00101f901cbd4030000000000c0020792030835333931332b7573&apiKey=${key}`)
+
+
+//function to get geoid: use https://api.geoapify.com/v1/geocode
+    function getGeoId(place){
+        fetch(`https://api.geoapify.com/v1/geocode/search?text=${place}&apiKey=${key}`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            const id = data.features[0].properties.place_id
+            getLocalInformation(id)
+        })   
+    }
+
+
+getGeoId('baraboo')
+
+function getLocalInformation(id){
+
+fetch(`https://api.geoapify.com/v2/places?categories=airport,healthcare&filter=place:${id}&apiKey=${key}`)
 .then(function(response){
     return response.json()
 })
 .then(function(data){
-    console.log(data)
+    const locations = data.features
+    console.log(locations)
+    for(let i=0; i < locations.length; i++){
+        if (locations[i].properties.name === undefined){
+            console.log(locations[i].properties.address_line1)
+        }else{
+        console.log(locations[i].properties.name)
+        }
+    }
 })
+
+}
